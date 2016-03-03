103d6eb60e6ffc832abdd5f2bd27cfddaece7e17
Anastasia Kiseleva <kuzyun@mail.ru>
2016-03-03 13:12:24 +0300

    New formatter class and tests for it

---
+++ AdaptiveFormatterTest.js
@@ -0,0 +1,11 @@
+﻿describe('InteractiveDataDisplay.AdaptiveFormatter', function () {
+    it('0.0000nn___  ->  0.nn x 10^-mmm in segment case', function () {   
+        var format = new InteractiveDataDisplay.AdaptiveFormatter(0.000000001, 0.000003);
+        expect(format.toString(0.00000003)).toBe('0.030e-6');
+    });
+    it('0.0000nn___  ->  0.nn x 10^-mmm in array case', function () {
+        var array = [0.00000001, 0.000004, 0.0000054, 0.000005];
+        var format = new InteractiveDataDisplay.AdaptiveFormatter(array);
+        expect(format.toString(array[1])).toBe('4.00e-6');
+    });
+});
